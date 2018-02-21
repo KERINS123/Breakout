011a7c13fc7773f8e7cf361c07cbd7f5bc1a725c
KERINS123 <35769351+KERINS123@users.noreply.github.com>
2018-02-21 14:34:53 +0000

    Move the ball P1
    Continuous line

--- js/breakout.js
+++ js/breakout.js
@@ -3,17 +3,26 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 //set the starting point
-var x = canvas.width/2;
-var y = canvas.height-30;
-
+var x= canvas.width/2;
+var y= canvas.height-30;
+var dx=2;
+ var dy=-2;
+ 
 //draw ball
-function draw(){
+function drawBall(){
+	x += dx;
+	y += dy;
 	ctx.beginPath();
 	ctx.arc(x, y, 10, 0, Math.PI*2);
 	ctx.fillStyle = '#0095DD';
 	ctx.fill();
 	ctx.closePath();
 }
+function draw() {
+	ctx.clearRect(0, 0, canvas.width ,canvas.height);
+	drawBall();
+	
+}
 
-setInterval(draw, 10);
+setInterval(drawBall, 10);
  
